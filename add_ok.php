49a5486f40870850a2ad91677f2ef4d7fdd04861
KangYewon <ooooo712@naver.com>
2023-10-07 15:56:16 +0900

    view.html

--- add_ok.php
+++ add_ok.php
@@ -2,19 +2,15 @@
     $word = $_POST['word'];
     $meaning = $_POST['meaning'];
     $level = $_POST['level'];
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h3>입력하신 단어는 다음과 같습니다 </h3>
-    <div>Word : <?=$word?></div>
-    <div>Meaning : <?=$meaning?></div>
-    <div>Level : <?=$level?></div>
-</body>
-</html>
+    $formData = array(
+        'word' => $word,
+        'meaning' => $meaning,
+        'level' => $level
+    );
+
+    echo "<script>
+            localStorage.setItem('formData', JSON.stringify(" . json_encode($formData) . "));
+            window.location.href = 'view.html';
+          </script>";
+?>
