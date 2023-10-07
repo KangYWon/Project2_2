0037524461194454c029e778e31a67267742cd36
KangYewon <ooooo712@naver.com>
2023-10-07 22:06:12 +0900

    대대적인 수정

--- add_ok.php
+++ add_ok.php
@@ -2,16 +2,19 @@
     $word = $_POST['word'];
     $meaning = $_POST['meaning'];
     $level = $_POST['level'];
-
-    $formData = array(
-        'word' => $word,
-        'meaning' => $meaning,
-        'level' => $level
-    );
-
-    echo "<script>
-            localStorage.setItem('formData', JSON.stringify(" . json_encode($formData) . "));
-            window.location.href = 'view.html';
-          </script>";
-    exit; // PHP 실행 종료
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>입력하신 데이터는 다음과 같습니다. </h3>
+    <div>Word : <?=$word?> </div>
+    <div>Meaning : <?=$meaning?> </div>
+    <div>Level : <?=$level?> </div>
+</body>
+</html>
